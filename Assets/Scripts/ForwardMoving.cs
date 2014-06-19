66a05d22d6de061efb2a3d40faf32a013a3272e7
anusen <a.m.krause93@googlemail.com>
2014-06-19 14:48:32 +0200

    forward movement and camera looking
    added script LookedCamera
    added script ForwardMoving

---
+++ Assets/Scripts/ForwardMoving.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// This script lets the player continuously move forward.
+// The distance and speed are variable.
+// It uses the CharacterControler SimpleMove function.
+
+
+public class ForwardMoving : MonoBehaviour {
+	
+	public Vector3 direction =				
+		new Vector3(-1,0,0);
+	public float speed = 5.0f;
+	
+	// Update is called once per frame
+	void Update () {
+		CharacterController controller = 				// gets the CharacterController for the player	
+			GetComponent<CharacterController>();
+		Vector3 movement = direction;					// gets the direction to move
+		movement.Normalize ();							// normalizes the direction to use only the 
+														// speed variable as speed factor
+		movement *= speed;								// multiplicats the speed to the direction
+		controller.SimpleMove (movement); 				// moves the player
+	}
+}
---
+++ Assets/Scripts/LookedCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// This script looks the camera to a fixed position, looking at the target.
+// The position is depending on the targets location.
+// Vector3 distance sets the distance between camera and target.
+
+public class LookedCamera : MonoBehaviour {
+
+	public GameObject target;							
+	public Vector3 distance =
+		new Vector3(5,5,0);
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		Vector3 campos = target.transform.position;			// getting target position
+		campos += distance;									// add distance
+		Camera.main.transform.position = campos;			// set camera position
+		Camera.main.transform.LookAt (target.transform);  	// let camera look at target
+	}
+}
